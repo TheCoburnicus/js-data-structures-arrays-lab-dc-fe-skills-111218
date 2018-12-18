da8b586ee957e7758e8d8c956f446c142de3821a
Nathan Coburn <nathanlcoburn@gmail.com>
2018-12-18 01:04:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your solution here!
+const drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name) {
+  drivers.shift(name);
+}
+
+let arr1 = [...arr2];
+
+function appendDriver(name) {
+  const copyOfDrivers = [...drivers, name];
+  return copyOfDrivers;
+}
+
+
+function prependDriver(name) {
+  const copyOfDrivers = [...drivers, name];
+  return copyOfDrivers;
+}
