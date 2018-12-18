929513bf9f757ce057d8f1a8a4658733ed553060
Nathan Coburn <nathanlcoburn@gmail.com>
2018-12-18 01:07:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function appendDriver(name) {
 }
 
 function prependDriver(name) {
-  const copyOfDrivers = [...drivers, NaN];
+  const copyOfDrivers = [...drivers, - name];
   return copyOfDrivers;
 }
